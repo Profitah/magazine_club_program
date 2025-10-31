9aa710c7bd585aed217d77f3d6000a28787c71bb
Roselle Yoonah Lee <101340482+Profitah@users.noreply.github.com>
2025-10-31 21:04:26 +0900

    [Feat] 회원 SNS 링크 입력 및 관리 기능 추가
    [feat] 회원 SNS 링크 입력 및 관리 기능 추가 #11

--- src/main/java/com/club/magazine_club_program/Service/MemberService.java
+++ src/main/java/com/club/magazine_club_program/Service/MemberService.java
@@ -14,14 +14,29 @@ public class MemberService {
         this.memberInfoMapper = memberInfoMapper;
     }
 
-    // 모든 멤버 조회
+    // 모든 멤버 조회 (SNS 링크 포함)
     public List<MemberDTO> getAllMembers() {
         return memberInfoMapper.findAll();
     }
 
+    // SNS 링크가 있는 멤버만 조회
+    public List<MemberDTO> getMembersWithSNS() {
+        return memberInfoMapper.findMembersWithSNS();
+    }
+    
     // 신규 멤버 추가
     public void addMember(MemberDTO memberDTO) {
         memberInfoMapper.addMember(memberDTO);
+    }
+
+    // 멤버 SNS 링크 업데이트
+    public boolean updateMemberSNS(MemberDTO memberDTO) {
+        return memberInfoMapper.updateMemberSNS(memberDTO) > 0;
+    }
+
+    // 멤버 SNS 링크 삭제
+    public boolean deleteMemberSNS(int id) {
+        return memberInfoMapper.deleteMemberSNS(id) > 0;
     }
 
     // 멤버 삭제
